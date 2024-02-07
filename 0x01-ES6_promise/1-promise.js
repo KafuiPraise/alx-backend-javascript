9e092f1750bb9062e7067055af206a6daee33e70
KafuiPraise <matildapraise@gmail.com>
2024-02-07 18:40:55 +0000

    Promise

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,9 @@
+/*
+ Using the prototype below, return a promise
+*/
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
+}
